4867b85621f322377e72432ebf0b8261d6c2fbb0
maxnvdm <maxnvdm@gmail.com>
2019-12-06 10:23:03 +0200

    Connected to mongo cloud database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,47 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'code todo list'}];
+// Connect to the database
+mongoose.connect('mongodb+srv://test:test@todo-kofr6.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected');
+});
+
+// Create a schema - blueprint for data
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to the view
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+        // get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data)
+        });
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        // delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).deleteOne(function(err, data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
-
-};
+}
