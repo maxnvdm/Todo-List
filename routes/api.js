ba930d5ef037ad1eab56d456fb6ab83b01db070d
maxnvdm <maxnvdm@gmail.com>
2019-12-09 18:59:02 +0200

    Adding instruction modal

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
 var Todo = require('../models/item');
+var bodyParser = require('body-parser');
+var bodyjson = bodyParser.json();
 
 // end-point for api/ get request, returns json of items in todo-list
 router.get('/', function(req, res){
@@ -11,11 +12,27 @@ router.get('/', function(req, res){
     });
 });
 
-// end-point for creating an item via the url /api/item and adds the item to the database
-router.post('/:item', function(req, res){
-    var newTodo = Todo({item: req.params.item}).save(function(err, data){
+// end-point for creating an item via posting a json in the following format {"item": "name"}
+router.post('/', bodyjson, function(req, res){
+    console.log(req.body)
+    // var item = new Todo(req.body);
+    var newTodo = Todo(req.body).save(function(err, data){
         if (err) throw err;
         res.json(data);
+    });
+});
+
+// end-point to delete an item using it's ID as found in the database (can also be found using the get API call)
+router.delete('/:item', function(req, res){
+    Todo.findByIdAndRemove({_id: req.params.item}).then(function(todo){
+        res.send(todo);
+    });
+});
+
+// end-point to update an item in the database using it's ID and entering the updated params in the body as a json
+router.put('/:item', bodyjson, function(req, res, next ){
+    Todo.findByIdAndUpdate({_id: req.params.item}, req.body, {new: true}).then(function(todo){
+        res.send(todo);
     });
 });
 
