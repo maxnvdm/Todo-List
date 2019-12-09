81f3e8f206460c706f8855ac934d91451bca5ad8
maxnvdm <maxnvdm@gmail.com>
2019-12-09 12:05:47 +0200

    Configuring port and .json for heroku deployment

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ todoController(app);
 app.use('/api', apiRoutes);
 
 // listen to port
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 console.log('Listening to port 3000');
